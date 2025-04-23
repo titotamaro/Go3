f03c6744f4c273883bccf2690f90c762fc4a8285
Tito Tamaro <tito.tamaro2@gmail.com>
2025-04-23 09:05:23 +0700

    Third commit

--- bank.go
+++ bank.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
 )
+
+const accountBalanceFile = "balance.txt"
+
+func getBalanceFromFile() (float64, error) {
+	data, err := os.ReadFile(accountBalanceFile)
+
+	if err != nil {
+		return 1000, errors.New("Failed to find balance file")
+	}
+
+	balanceText := string(data)
+	balance, err := strconv.ParseFloat(balanceText, 64)
+
+	if err != nil {
+		return 1000, errors.New("Failed to parse stored balance value")
+	}
+	return balance, nil
+}
 
 func writeBalanceToFile(balance float64) {
 	balanceText := fmt.Sprint(balance)
@@ -11,7 +31,13 @@ func writeBalanceToFile(balance float64) {
 }
 
 func main() {
-	var accountBalance = 1000.0
+	var accountBalance, err = getBalanceFromFile()
+
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		fmt.Println("----------")
+	}
 
 	fmt.Println("Welcome to Go Bank!")
 
